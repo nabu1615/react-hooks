5e5881e4f2885a16c87a68be435be2946ae91e11
Javier Vargas <javier1615@gmail.com>
2021-04-11 07:16:03 -0500

    Logic to fetch and add a favorite character

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,70 +1,72 @@
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useEffect, useState, useReducer } from "react";
 
 const initialState = {
-    favorites: []
-}
+  favorites: [],
+};
 
 const favoriteReduce = (state, action) => {
-    switch (action.type) {
-        case 'ADD_TO_FAVORITE':
-            return {
-                ...state,
-                favorites: [...state.favorites, action.payload]
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "ADD_TO_FAVORITE":
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload],
+      };
+    default:
+      return state;
+  }
+};
 
 const Characters = () => {
-    const [characters, setCharacters] = useState([]);
-    const [favorites, dispatch] = useReducer(favoriteReduce, initialState);
+  const [characters, setCharacters] = useState([]);
+  const [favorites, dispatch] = useReducer(favoriteReduce, initialState);
 
-    useEffect(() => {
-        fetch('https://rickandmortyapi.com/api/character/')
-            .then(response => response.json())
-            .then(data => setCharacters(data.results))
-    }, []);
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character/")
+      .then((response) => response.json())
+      .then((data) => setCharacters(data.results));
+  }, []);
 
-    const handleClick = favorite => {
-        dispatch({type: 'ADD_TO_FAVORITE', payload: favorite})
-    }
+  const handleClick = (favorite) => {
+    dispatch({ type: "ADD_TO_FAVORITE", payload: favorite });
+  };
 
-    console.log(favorites);
+  console.log(favorites);
 
-    return (
-        <div className="Characters">
-            { favorites.favorites.length ?
-            <ul className='Favorites'>
-                <h3>Favorite Characters</h3>
-                {favorites.favorites.map(favorite => {
-                    return (
-                        <li  key={favorite.id}>
-                            <h5>{ favorite.name} </h5>
-                        </li>
-                    )
-                })}
-            </ul>
-            : ''
-            }
+  return (
+    <div className="Characters">
+      {favorites.favorites.length ? (
+        <ul className="Favorites">
+          <h3>Favorite Characters</h3>
+          {favorites.favorites.map((favorite) => {
+            return (
+              <li key={favorite.id}>
+                <h5>{favorite.name} </h5>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        ""
+      )}
 
-
-            <div className="CharactersList">
-                {
-                    characters.map(character => {
-                        return (
-                            <div className="Character" key={character.id}>
-                                <img src={character.image} alt=""/>
-                                <h3>{ character.name} </h3>
-                                <small><b>Gender:</b> {character.gender}</small>
-                                <button onClick={() => handleClick(character)}>Add to favorite</button>
-                            </div>
-                        )
-                    })
-                }
+      <div className="CharactersList">
+        {characters.map((character) => {
+          return (
+            <div className="Character" key={character.id}>
+              <img src={character.image} alt="" />
+              <h3>{character.name} </h3>
+              <small>
+                <b>Gender:</b> {character.gender}
+              </small>
+              <button onClick={() => handleClick(character)}>
+                Add to favorite
+              </button>
             </div>
-        </div>
-    )
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Characters;
